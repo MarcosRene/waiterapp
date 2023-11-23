79c9471d60fed5800c48e15c984ee32afe529310
Marcos Renê <renemoura789@gmail.com>
2023-11-23 15:56:38 -0300

    feat: add listing and creating methods to category and products

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,16 @@
+import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { router } from './router';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(express.json());
+    app.use(router);
 
     app.listen(3333, () => {
       console.log('🚀 Server is running on http://localhost:3333');
--- api/src/router.ts
+++ api/src/router.ts
@@ -1,22 +1,33 @@
+import path from 'node:path';
+
 import { Router } from 'express';
+import multer from 'multer';
+
+import { listCategories } from './app/useCases/categories/listCategories';
+import { createCategory } from './app/useCases/categories/createCategory';
+import {  listProducts } from './app/useCases/produtcs/listProducts';
+import { createProduct } from './app/useCases/produtcs/createProduct';
 
 export const router = Router();
 
-router.get('/categoroies', (req, res) => {
-  res.send('ok');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    }
+  })
 });
 
-router.post('/categoroies', (req, res) => {
-  res.send('ok');
-});
+router.get('/categories', listCategories);
 
-router.get('/products', (req, res) => {
-  res.send('ok');
-});
+router.post('/categories', createCategory);
 
-router.post('/products', (req, res) => {
-  res.send('ok');
-});
+router.get('/products', listProducts);
+
+router.post('/products', upload.single('image'), createProduct);
 
 router.get('/categoreis/:categoryId/products', (req, res) => {
   res.send('ok');
