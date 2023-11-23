ba179847561f5d4ca50892f9c65a1e38d8df7edc
Marcos Renê <renemoura789@gmail.com>
2023-11-23 17:00:32 -0300

    change: add styled-components and global styles reset

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 function App() {
-  return <h1>Marcos</h1>;
+  return(
+    <>
+      <GlobalStyles/>
+      <h1>Marcos</h1>
+    </>
+  );
 }
 
 export default App;
