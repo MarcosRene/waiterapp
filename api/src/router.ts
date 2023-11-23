570f4b52c998f10da4d0233b0a3d69f544f21295
Marcos Renê <renemoura789@gmail.com>
2023-11-23 16:07:43 -0300

    feat: get product by category

--- api/src/router.ts
+++ api/src/router.ts
@@ -7,6 +7,7 @@ import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
 import {  listProducts } from './app/useCases/produtcs/listProducts';
 import { createProduct } from './app/useCases/produtcs/createProduct';
+import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 
 export const router = Router();
 
@@ -29,9 +30,7 @@ router.get('/products', listProducts);
 
 router.post('/products', upload.single('image'), createProduct);
 
-router.get('/categoreis/:categoryId/products', (req, res) => {
-  res.send('ok');
-});
+router.get('/categories/:categoryId/products', listProductsByCategory);
 
 router.get('/orders', (req, res) => {
   res.send('ok');
