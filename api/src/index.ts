129385bbc5a1af907197cad85fedb9bca7cb98a8
Marcos Renê <renemoura789@gmail.com>
2023-11-23 12:36:32 -0300

    feat: add config mongo to api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-app.listen(3333, () => {
-  console.log('🚀 Server is running on http://localhost:3333');
-});
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+
+    app.listen(3333, () => {
+      console.log('🚀 Server is running on http://localhost:3333');
+    });
+  })
+  .catch(() => { console.log('Erro ao connectar no mongodb'); });
+
+
